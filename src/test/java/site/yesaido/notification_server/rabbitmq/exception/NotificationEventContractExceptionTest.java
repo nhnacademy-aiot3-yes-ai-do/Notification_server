63ef93a2454c4865f82024911b96ee06fe4057a6
hetgwi01 <hetgwi03@gmail.com>
2026-08-13 01:22:14 +0000

    test(rabbitmq): add unit tests for notification event contract exceptions
    - Verify that missing notification event types instantiate CustomServerException with ERROR_LEVEL severity
    - Verify that event target type mismatches are correctly identified as server contract errors with ERROR_LEVEL
    - Ensure RabbitMQ contract violation exceptions properly declare custom server error hierarchy and severity level

--- src/test/java/site/yesaido/notification_server/rabbitmq/exception/NotificationEventContractExceptionTest.java
+++ src/test/java/site/yesaido/notification_server/rabbitmq/exception/NotificationEventContractExceptionTest.java
@@ -1,4 +1,4 @@
-package site.yesaido.notification_server.exception.event;
+package site.yesaido.notification_server.rabbitmq.exception;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
